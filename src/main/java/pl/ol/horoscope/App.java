aef7fbe4ebb61f41aa2f178cda88608b9d9ce453
Aleksandra Szafruga <ola.szafruga@gmail.com>
2020-02-02 15:17:01 +0100

    Clearing text

--- src/main/java/pl/ol/horoscope/App.java
+++ src/main/java/pl/ol/horoscope/App.java
@@ -5,19 +5,30 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        System.out.println("Enter the zodiac sign in lowercase.");
-        Scanner scanner = new Scanner(System.in);
-        String enterZodiacSign = scanner.next();
-        String requestResult = Connector.getPage(enterZodiacSign);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hi, what's your name?");
+        String name = sc.nextLine();
+        System.out.println("What day of the month were you born?");
+        Integer dayOfMonth = sc.nextInt();
+        sc.nextLine();
+        System.out.println("What month of the year were you born?");
+        Integer monthOfYear = sc.nextInt();
+        sc.nextLine();
+
+        String requestResult = ZodiacSignRecogniser.getSign(dayOfMonth, monthOfYear);
+        String massage = Connector.getPage(requestResult);
         String horoscopeMessage = "";
 
         if (!requestResult.equals(Connector.NOT_FOUND)) {
-            horoscopeMessage = HoroscopeText.getHoroscopeText(requestResult);
+            horoscopeMessage = HoroscopeText.getHoroscopeText(massage);
         }
         System.out.println(horoscopeMessage);
 
+
     }
-    private static void testHoroscopeExtractor(){
+
+    private static void testHoroscopeExtractor() {
         String horoscopeText = HoroscopeText.getHoroscopeText("TRALALALA LALA!");
         System.out.println(horoscopeText);
-}}
+    }
+}
